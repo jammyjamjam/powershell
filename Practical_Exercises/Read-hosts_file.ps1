42510c2559bcefa9538baf293cf402386abc18f2
jammyjamjam <t.james1017@gmail.com>
2023-02-15 08:49:30 -0500

    updated readhost

--- Practical_Exercises/Read-hosts_file.ps1
+++ Practical_Exercises/Read-hosts_file.ps1
@@ -1,10 +1,9 @@
 function read-hosts_file {
 
-    $ip_addresses = Get-Content C:\windows\System32\drivers\etc\hosts | select-string -Pattern "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" | foreach {$_.matches} | select value | sort
+    $ip_addresses_in_host_file = @()
+    $ip_addresses_in_host_file = Get-Content C:\windows\System32\drivers\etc\hosts | select-string -Pattern "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" | foreach {$_.matches} | select value
 
-    #$a_class = 
-    #$b_class = 
-    #$c_class =
+    write-host $ip_addresses_in_host_file
 
 }
 
